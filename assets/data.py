5299acdc4473c0587c11d8f86d8e26ceff0096d1
Francisco Griman <103836660+fcoagz@users.noreply.github.com>
2022-11-14 23:03:45 -0400

    📩 Nuevo Script donde sera recibido el format json online

---
+++ assets/data.py
@@ -0,0 +1,33 @@
+import json
+from urllib import request
+
+CONECTION = {
+    'url': 'https://my-json-server.typicode.com/Nopal-vi/Nopal-viTools/update',
+    'message': 'MENSAJE:',
+    'error_conection': 'SIN CONEXION a INTERNET',
+    'version': 'VERSION ACTUAL:',
+    'update': 'VERSION NECESARIA:'
+}
+def client_message(): # mensaje recibido por un formato json
+    try:
+        url = '%s' % (CONECTION['url'])
+        post = request.urlopen(url)
+        content = post.read()
+
+        decode_json = json.loads(content.decode('utf-8'))
+        for url in decode_json:
+            print(f"%s {url['title']}" % (CONECTION['message']))
+    except:
+        print(CONECTION['message'], CONECTION['error_conection'])
+
+def client_version(): # mensaje sobre la version necesaria
+    try:
+        url = '%s' % (CONECTION['url'])
+        post = request.urlopen(url)
+        content = post.read()
+
+        decode_json = json.loads(content.decode('utf-8'))
+        for url in decode_json:
+            print(f"%s {url['about']}" % (CONECTION['update']))
+    except:
+        print('%s %s' % (CONECTION['message', 'error_conection']))
