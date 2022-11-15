2c51ad6730e5185004852067df0259a84ab76163
Francisco Griman <103836660+fcoagz@users.noreply.github.com>
2022-11-14 22:59:25 -0400

    👾 Implementando nuevo módulos y orden en sus funciones

--- main.py
+++ main.py
@@ -1,50 +1,210 @@
-import data
+from assets import data
+from assets import locate
 import os
-import progress
-import time
 
-os.system('title Nopal-viTools - Nopal-vi Organization')
-information = 'Nopal-viTools [Version 0.5] - Nopal-vi Organization'
+about = {'title': 'Nopal-viTools [Version 0.6] - Nopal-vi Organization'}
 
-def main_update():
-    clear()
-    data.main_i()
-    print(information)
-    print('')
-    print('Nopal-viTools es una herramienta utilizando la documentacion de FFmpeg\n\
-para la conversion de video en (.avi) sea disponible en Tuna-viDS')
-    print('')
-    print(' 1- 256x192: Resolucion de pantalla completa')
-    print(' 2- 256x144: Recomendado por usuarios de Nopal-viTools')
-    print('')
-    print(' 3- Update: Buscar actualizaciones...')
-    home = input('>>> ')
-    
-    if home == '1':
-        progress.resolution_x()
-    if home == '2':
-        progress.resolution_y()
+def client_start():
+    print('%s' % (about['title']))
+    lenguage = str(input('''select a language:
+ES: Spanish
+EN: English
+-> '''))
+    if lenguage == 'ES':
+        def ES():
+            paper()
+            data.client_message()
+            print('%s' % (about['title']),'\n')
+            print(locate.ES.NOPAL_VI,'\n','\n',
+            locate.ES.xvids,'\n',
+            locate.ES.xvidsi, '\n')
+            xvid = str(input('-> '))
 
-    if home == '3':
-        print('')
-        data.update_json()
-        print('VERSION ACTUAL: Nopal-viTools [Version 0.5]')
-        print('')
-        print(' 1- Volver menu principal')
-        home = input('>>> ')
-        if home == '1':
-            main_update()
-        else:
-            print('Hubo un error, comando no disponible!')
+            if xvid == '1':
+                print('\n',
+                locate.ES.RESOLUTION_IN_xvids, '\n')
+                format = str(input('-> '))
+
+                if format == 0:
+                    pass
+                elif format == '1':
+                    def xvids_1():
+                        try:
+                            os.system(f'%s{locate.xvids_MP4} {locate.OUPUT_AVI}' % (locate.LOCAL['ffmpeg']))
+                            print(locate.ES.COMPLETE)
+                            print(os.system('pause >nul'), ES())
+                        except:
+                            print("'ffmpeg' no se reconoce, ve a github.com/Nopal-vi/Nopal-viTools")
+                            print(os.system('pause >nul'), ES())
+                    xvids_1()
+                elif format == '2':
+                    try:
+                        os.system(f'%s{locate.xvids_AVI} {locate.OUPUT_AVI}' % (locate.LOCAL['ffmpeg']))
+                        print(locate.ES.COMPLETE)
+                        print(os.system('pause >nul'), ES())
+                    except:
+                        print('%s' % (locate.LOCAL['error_ffmpeg']))
+                        print(os.system('pause >nul'), ES())
+                elif format == '3':
+                    try:
+                        os.system(f'%s{locate.xvids_MOV} {locate.OUPUT_AVI}' % (locate.LOCAL['ffmpeg']))
+                        print(locate.ES.COMPLETE)
+                        print(os.system('pause >nul'), ES())
+                    except:
+                        print('%s' % (locate.LOCAL['error_ffmpeg']))
+                        print(os.system('pause >nul'), ES())
+                elif format == '4':
+                    try:
+                        os.system(f'%s{locate.xvids_WMV} {locate.OUPUT_AVI}' % (locate.LOCAL['ffmpeg']))
+                        print(locate.ES.COMPLETE)
+                        print(os.system('pause >nul'), ES())
+                    except:
+                        print('%s' % (locate.LOCAL['error_ffmpeg']))
+                        print(os.system('pause >nul'), ES())
+                else:
+                    print('%s' % (locate.LOCAL['int_error']))
+            elif xvid == '2':
+                print('\n',
+                locate.ES.RESOLUTION_IN_xvids, '\n')
+                format = str(input('-> '))
+
+                if format == 0:
+                    pass
+                elif format == '1':
+                    try:
+                        os.system(f'%s{locate.xvidsi_MP4} {locate.OUPUT_AVI}' % (locate.LOCAL['ffmpeg']))
+                        print(locate.ES.COMPLETE)
+                        print(os.system('pause >nul'), ES())
+                    except:
+                        print('%s' % (locate.LOCAL['error_ffmpeg']))
+                        print(os.system('pause >nul'), ES())
+                elif format == '2':
+                    try:
+                        os.system(f'%s{locate.xvidsi_AVI} {locate.OUPUT_AVI}' % (locate.LOCAL['ffmpeg']))
+                        print(locate.ES.COMPLETE)
+                        print(os.system('pause >nul'), ES())
+                    except:
+                        print('%s' % (locate.LOCAL['error_ffmpeg']))
+                        print(os.system('pause >nul'), ES())
+                elif format == '3':
+                    try:
+                        os.system(f'%s{locate.xvidsi_MOV} {locate.OUPUT_AVI}' % (locate.LOCAL['ffmpeg']))
+                        print(locate.ES.COMPLETE)
+                        print(os.system('pause >nul'), ES())
+                    except:
+                        print('%s' % (locate.LOCAL['error_ffmpeg']))
+                        print(os.system('pause >nul'), ES())
+                elif format == '4':
+                    try:
+                        os.system(f'%s{locate.xvidsi_WMV} {locate.OUPUT_AVI}' % (locate.LOCAL['ffmpeg']))
+                        print(locate.ES.COMPLETE)
+                        print(os.system('pause >nul'), ES())
+                    except:
+                        print('%s' % (locate.LOCAL['error_ffmpeg']))
+                        print(os.system('pause >nul'), ES())
+                else:
+                    print('%s' % (locate.LOCAL['int_error']))
+            else:
+                print('%s' % (locate.LOCAL['int_error']))
+                print(os.system('pause >nul'), ES())
+        ES()
+
+    elif lenguage == 'EN':
+        def EN():
+            paper()
+            data.client_message()
+            print('%s' % (about['title']),'\n')
+            print(locate.EN.NOPAL_VI,'\n','\n',
+            locate.EN.xvids,'\n',
+            locate.EN.xvidsi, '\n')
+            xvid = str(input('-> '))
+
+            if xvid == '1':
+                print('\n',
+                locate.EN.RESOLUTION_IN_xvids, '\n')
+                format = str(input('-> '))
+
+                if format == '1':
+                    try:
+                        os.system(f'%s{locate.xvids_MP4} {locate.OUPUT_AVI}' % (locate.LOCAL['ffmpeg']))
+                        print(locate.EN.COMPLETE)
+                        print(os.system('pause >nul'), EN())
+                    except:
+                        print('%s' % (locate.LOCAL['error_ffmpeg']))
+                elif format == '2':
+                    try:
+                        os.system(f'%s{locate.xvids_AVI} {locate.OUPUT_AVI}' % (locate.LOCAL['ffmpeg']))
+                        print(locate.EN.COMPLETE)
+                        print(os.system('pause >nul'), EN())
+                    except:
+                        print('%s' % (locate.LOCAL['error_ffmpeg']))
+                elif format == '3':
+                    try:
+                        os.system(f'%s{locate.xvids_MOV} {locate.OUPUT_AVI}' % (locate.LOCAL['ffmpeg']))
+                        print(locate.EN.COMPLETE)
+                        print(os.system('pause >nul'), EN())
+                    except:
+                        print('%s' % (locate.LOCAL['error_ffmpeg']))
+                elif format == '4':
+                    try:
+                        os.system(f'%s{locate.xvids_WMV} {locate.OUPUT_AVI}' % (locate.LOCAL['ffmpeg']))
+                        print(locate.EN.COMPLETE)
+                        print(os.system('pause >nul'), EN())
+                    except:
+                        print('%s' % (locate.LOCAL['error_ffmpeg']))
+                else:
+                    print('%s' % (locate.LOCAL['int_error']))
+            elif xvid == '2':
+                print('\n',
+                locate.EN.RESOLUTION_IN_xvids, '\n')
+                format = str(input('-> '))
+
+                if format == '1':
+                    try:
+                        os.system(f'%s{locate.xvidsi_MP4} {locate.OUPUT_AVI}' % (locate.LOCAL['ffmpeg']))
+                        print(locate.EN.COMPLETE)
+                        print(os.system('pause >nul'), EN())
+                    except:
+                        print('%s' % (locate.LOCAL['error_ffmpeg']))
+                elif format == '2':
+                    try:
+                        os.system(f'%s{locate.xvidsi_AVI} {locate.OUPUT_AVI}' % (locate.LOCAL['ffmpeg']))
+                        print(locate.EN.COMPLETE)
+                        print(os.system('pause >nul'), EN())
+                    except:
+                        print('%s' % (locate.LOCAL['error_ffmpeg']))
+                elif format == '3':
+                    try:
+                        os.system(f'%s{locate.xvidsi_MOV} {locate.OUPUT_AVI}' % (locate.LOCAL['ffmpeg']))
+                        print(locate.EN.COMPLETE)
+                        print(os.system('pause >nul'), EN())
+                    except:
+                        print('%s' % (locate.LOCAL['error_ffmpeg']))
+                elif format == '4':
+                    try:
+                        os.system(f'%s{locate.xvidsi_WMV} {locate.OUPUT_AVI}' % (locate.LOCAL['ffmpeg']))
+                        print(locate.EN.COMPLETE)
+                        print(os.system('pause >nul'), EN())
+                    except:
+                        print('%s' % (locate.LOCAL['error_ffmpeg']))
+                else:
+                    print('%s' % (locate.LOCAL['int_error']))
+            else:
+                print('%s' % (locate.LOCAL['int_error']))
+                print(os.system('pause >nul'), EN())
+        EN()
+
     else:
-        print('Hubo un error, comando no disponible!')
-        time.sleep(2)
-        main_update()
-def clear():
-    os.system('cls')
+        print('selected language error = {}, to select a language, write ES or EN'.format(lenguage))
+        print(os.system('pause >nul'), client_start())
+        
+def paper():
+    try:
+        if os.system('cls') == 'Windows':
+            os.system('cls')
+    except:
+        if os.system('clear') == 'Linux':
+            os.system('clear')
 
 if __name__ == '__main__':
-    main_update()
-
-if __name__ == '__main__':
-    home_house()
+    client_start()
